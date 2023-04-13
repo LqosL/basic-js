f0ad572f8e9c09d085003f5562cfa9dbf2a96c3e
LqosL <camilla_agliullina@mail.ru>
2023-04-13 13:13:57 +0600

    Update carbon-dating.js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -18,7 +18,10 @@ const HALF_LIFE_PERIOD = 5730;
  *
  */
 function dateSample(sampleActivity) {
-  return Math.ceil((Math.log(MODERN_ACTIVITY / sampleActivity)) /  (0.693 /HALF_LIFE_PERIOD))
+  if (typeof sampleActivity == "string" && !isNaN(sampleActivity) && parseInt(sampleActivity) && parseFloat(sampleActivity) > 0 && parseFloat(sampleActivity) <= 15){
+    return Math.ceil((Math.log(MODERN_ACTIVITY / sampleActivity)) /  (0.693 /HALF_LIFE_PERIOD))
+  }
+  return false
 }
 
 module.exports = {
